d1b860e0e40b06311899abc99bef8594d0668d2b
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-03-16 23:56:52 +0900

    Add a nest for spec

--- spec/lib/inline_template_loader_spec.rb
+++ spec/lib/inline_template_loader_spec.rb
@@ -2,20 +2,22 @@ require 'inline_template_loader'
 
 describe 'InlineTemplateLoader' do
   describe '#load' do
-    shared_examples_for 'load templates correctly from this file' do
-      it { should == {foo: "Here is template named foo.\n\n", bar: "Here is template named bar.\n"} }
-    end
+    context 'from this file' do
+      shared_examples_for 'load templates correctly from this file' do
+        it { should == {foo: "Here is template named foo.\n\n", bar: "Here is template named bar.\n"} }
+      end
 
-    context 'with filepath' do
-      subject { InlineTemplateLoader.load(__FILE__) }
+      context 'with filepath' do
+        subject { InlineTemplateLoader.load(__FILE__) }
 
-      it_behaves_like 'load templates correctly from this file'
-    end
+        it_behaves_like 'load templates correctly from this file'
+      end
 
-    context 'without filepath' do
-      subject { InlineTemplateLoader.load }
+      context 'without filepath' do
+        subject { InlineTemplateLoader.load }
 
-      it_behaves_like 'load templates correctly from this file'
+        it_behaves_like 'load templates correctly from this file'
+      end
     end
   end
 end
