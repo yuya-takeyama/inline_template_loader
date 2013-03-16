9372f25dbf90b479280b50bbe3fd09a1d23d191b
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-03-16 23:02:12 +0900

    Implement without filepath

--- lib/inline_template_loader.rb
+++ lib/inline_template_loader.rb
@@ -1,7 +1,11 @@
 require "inline_template_loader/version"
 
 module InlineTemplateLoader
-  def self.load(file)
+  def self.load(file = nil)
+    if file.nil?
+      file = caller.first.split(':').first
+    end
+
     templates = {}
     sym = nil
 
--- spec/lib/inline_template_loader_spec.rb
+++ spec/lib/inline_template_loader_spec.rb
@@ -2,9 +2,21 @@ require 'inline_template_loader'
 
 describe 'InlineTemplateLoader' do
   describe '#load' do
-    subject { InlineTemplateLoader.load(__FILE__) }
+    shared_examples_for 'load templates correctly from this file' do
+      it { should == {foo: "Here is template named foo.\n\n", bar: "Here is template named bar.\n"} }
+    end
 
-    it { should == {foo: "Here is template named foo.\n\n", bar: "Here is template named bar.\n"} }
+    context 'with filepath' do
+      subject { InlineTemplateLoader.load(__FILE__) }
+
+      it_behaves_like 'load templates correctly from this file'
+    end
+
+    context 'without filepath' do
+      subject { InlineTemplateLoader.load }
+
+      it_behaves_like 'load templates correctly from this file'
+    end
   end
 end
 
